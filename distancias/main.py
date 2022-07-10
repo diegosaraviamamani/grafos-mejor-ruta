5fb4a463cbc3f16e8df81ad708801f732392a38b
Diego Saravia Mamani <63614444+diegosaraviamamani@users.noreply.github.com>
2022-07-09 23:43:14 -0400

    matriz de distancias

--- distancias/main.py
+++ distancias/main.py
@@ -1,29 +1,20 @@
 from AlgoritmoTSP import AlgoritmoTSP
 from clases import Camino, Nodo
 
-nodoA = Nodo("A")
-nodoB = Nodo("B")
-nodoC = Nodo("C")
-nodoD = Nodo("D")
+# matriz de distancias
+matriz = [
+    [0, 9, 7, 8],
+    [9, 0, 10, 15],
+    [7, 10, 0, 4],
+    [8, 15, 4, 0]
+]
+grafo = [Nodo("A"), Nodo("B"), Nodo("C"), Nodo("D")]
 
-nodoA.caminos.append(Camino(nodoB, 9))
-nodoA.caminos.append(Camino(nodoC, 7))
-nodoA.caminos.append(Camino(nodoD, 8))
+for i in range(len(matriz)):
+    for j in range(len(matriz[i])):
+        if matriz[i][j] != 0:
+            grafo[i].caminos.append(Camino(grafo[j], matriz[i][j]))
 
-nodoB.caminos.append(Camino(nodoA, 9))
-nodoB.caminos.append(Camino(nodoC, 10))
-nodoB.caminos.append(Camino(nodoD, 15))
-
-nodoC.caminos.append(Camino(nodoA, 7))
-nodoC.caminos.append(Camino(nodoB, 10))
-nodoC.caminos.append(Camino(nodoD, 4))
-
-nodoD.caminos.append(Camino(nodoA, 8))
-nodoD.caminos.append(Camino(nodoB, 15))
-nodoD.caminos.append(Camino(nodoC, 4))
-
-grafo = [nodoA, nodoB, nodoC, nodoD]
-
-tsp = AlgoritmoTSP(grafo, 10, nodoB)
+tsp = AlgoritmoTSP(grafo, 10, grafo[3])
 tsp.caminar()
 print(tsp.todasLasRutas)
